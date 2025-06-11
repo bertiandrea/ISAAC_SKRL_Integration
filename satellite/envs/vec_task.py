ba5b0035c82da81833a1a94b8e2e58b0cf079374
andreaberti <andreaberti410@gmail.com>
2025-06-11 14:22:12 +0200

    COMMIT

--- satellite/envs/vec_task.py
+++ satellite/envs/vec_task.py
@@ -122,17 +122,22 @@ class VecTask(Params):
 
     def step(self, actions: torch.Tensor) -> Tuple[Dict[str, torch.Tensor], torch.Tensor, torch.Tensor, Dict[str, Any]]:
         with record_function("VecTask__step"):
-            self.pre_physics_step(actions)
+            with record_function("VecTask__step__pre_physics_step"):
+                self.pre_physics_step(actions)
 
             ######################################################################
-            if not self.headless and self.device_type != 'cpu':
-                self.render()
-            self.gym.simulate(self.sim)
-            if self.device_type == 'cpu':
-                self.gym.fetch_results(self.sim, True)
+            with record_function("VecTask__step__RENDER"):
+                if not self.headless and self.device_type != 'cpu':
+                    self.render()
+            with record_function("VecTask__step__SIMULATE"):
+                self.gym.simulate(self.sim)
+            with record_function("VecTask__step__FETCH_RESULTS"):
+                if self.device_type == 'cpu':
+                    self.gym.fetch_results(self.sim, True)
             ######################################################################
             
-            self.post_physics_step()
+            with record_function("VecTask__step__post_physics_step"):
+                self.post_physics_step()
         
         return self.states_buf, \
             self.reward_buf.view(-1, 1), \
